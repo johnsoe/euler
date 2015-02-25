7f03d0f30301927bde16774c4e733b4175a8e322
johnsoe <evan.johnson.r@gmail.com>
2015-02-24 20:00:19 -0800

    Problem 16

--- js/euler016.js
+++ js/euler016.js
@@ -1,17 +1,32 @@
+//assume value is a string representation of a number
 var digitSum = function(value) {
-	if(value < 0) value *= -1;
 	var sum = 0;
-	while(value > 0) {
-		sum += value % 10;
-		value = Math.floor(value / 10);
+	for(var i = 0; i < value.length; i++) {
+		sum += parseInt(value.charAt(i));
 	}
 	return sum;
-}
-
-var printExponents = function() {
-	for(var i = 0; i < 2000; i++) {
-		debug(i + " " +digitSum(Math.pow(2, i)));
-	}
 };
 
-printExponents();
+//2^1000 is too large for a javascript number representation. 
+//Instead do string multiplication.
+var stringMultiply = function(value, multiplier) {
+	var result = "";
+	var carry = 0;
+	for (var i = value.length - 1; i >= 0; i--) {
+		var digit = parseInt(value.charAt(i));
+		var singleResult = digit * multiplier + carry;
+		carry = Math.floor(singleResult / 10);
+		result = singleResult % 10 + result;
+	}
+	return carry == 0 ? result: carry + result;
+};
+
+var calcPower = function(base, power) {
+	var result = "1";
+	for (var i = 1; i <= power; i++) {
+		result = stringMultiply(result, base);
+	}
+	return result;
+}
+
+debug(digitSum(calcPower(2, 1000)));
